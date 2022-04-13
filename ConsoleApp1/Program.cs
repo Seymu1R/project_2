4c880142a7a7ae6bb359b65a59852d4eabe3819b
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-04-13 17:42:22 +0400

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Include First number");
+            int number_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Include second number ");
+            int number_2 = Convert.ToInt32(Console.ReadLine());
+            if (number_1 > number_2) {
+                Console.WriteLine("First number is larger than second number");
+
+            }
+            else if (number_1 == number_2) {
+                Console.WriteLine("First number is equal to second number");
+            }
+            else {
+                Console.WriteLine("Second number is larger than first number"); 
+            }
+            
+            
+            
+            
+           
+
+
+        }
+    }
+}
